dc4d29d4b475fbb016f50d0bbecb3d8c37e295f4
Julio <jucesargois@gmail.com>
2021-10-28 16:25:36 -0300

    Created MenuCalc
    to show the options menu for the user on the terminal

---
+++ lib/src/menu_calculadora.dart
@@ -0,0 +1,12 @@
+MenuCalc() {
+  print(
+      "Escolha a operação matemática para realizar o calculo de dois valores:\n");
+  print("soma");
+  print("subtracao");
+  print("divisao");
+  print("multiplicacao");
+  print("---------------------------------------");
+  print("Digite SAIR para finalizar o programa");
+  print("---------------------------------------");
+  print("Qual opções deseja escolher:\n ");
+}
