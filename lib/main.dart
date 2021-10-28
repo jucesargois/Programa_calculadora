6edcf36da1e2e8651b2c088211fa075077e9904b
Julio <jucesargois@gmail.com>
2021-10-28 16:22:01 -0300

    Added call to MenuCalc and creating variables to be used in other files

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,15 @@
+import 'dart:io';
+import 'package:programa_calculadora/src/calculadora.dart';
+import 'package:programa_calculadora/src/interface.dart';
+import 'package:programa_calculadora/src/menu_calculadora.dart';
 
+String? opcoes;
+bool condicao = true;
+String? yorn;
 main(List<String> arguments) {
-  
+  var n1;
+  var n2;
+  MenuCalc();
+  opcoes = stdin.readLineSync();
+  StructureCalc(condicao, opcoes, n1, n2, yorn);
 }
